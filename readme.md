5e0b944d904ac608303c470719df644890301198
flo <florian.schildwaechter@gmail.com>
2024-01-12 15:00:53 +0100

    add readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+# WTF is BackInTime? 
+
+> It is a web app written in [Golang](https://go.dev/), [Templ](https://templ.guide/) and [HTMX](https://htmx.org/) because I wanted to try out this stack. 
+
+Actually it is a game in which you connect to your [Spotify](spotify.com) account and then basically have to guess the correct release year order of songs from any Spotify playlist you can provide.
+
+![This is what it currently looks like!](./screenshot.png "This is what it currently looks like")
--- src/templates/components/game_templ.go
+++ src/templates/components/game_templ.go
@@ -76,7 +76,7 @@ func Game(playlistUri string, gameEntry services.GameEntry) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<section class=\"flex gap-8\"><div class=\"card-wrapper flex flex-none gap-8 sortable h-screen\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<section class=\"flex gap-6\"><div class=\"card-wrapper flex flex-wrap gap-6 sortable h-dvh\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
